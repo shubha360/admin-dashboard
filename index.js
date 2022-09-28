7953ee929e757c3c3d6561e2fa08087a93f5bfe5
Raquibul Islam <shubha360@outlook.com>
2022-09-28 17:00:00 +0600

    complete

---
+++ index.js
@@ -0,0 +1,45 @@
+
+
+
+
+const headerPanel = document.querySelector("header");
+const mainPanel = document.querySelector("main");
+const navButton = document.querySelector(".top-panel #nav-button");
+
+const navItems = document.querySelectorAll("nav .nav-item");
+
+var navIsOpen = false;
+
+navButton.addEventListener("click", () => {
+	
+	headerPanel.style.display = "unset";
+	headerPanel.style.visibility = "unset";
+	
+	mainPanel.style.marginLeft = "60vw";
+	
+	navButton.style.display = "none";
+	navButton.style.visibility = "hidden";
+	
+	// mainPanel.addEventListener("click", closeNav);
+	
+	navIsOpen = true;
+});
+
+navItems.forEach(item => item.addEventListener("click", closeNav));
+
+
+function closeNav() {	
+	if (navIsOpen) {
+		headerPanel.style.display = "none";
+		headerPanel.style.visibility = "hidden";
+		
+		mainPanel.style.marginLeft = "0";
+		
+		navButton.style.display = "unset";
+		navButton.style.visibility = "unset";
+		
+		// mainPanel.removeEventListener("click");
+		
+		navIsOpen = false;
+	}
+}
